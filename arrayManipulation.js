c568135fec428164956c92977f998ca6dfd2edf2
ChrisKoram <cokoram001@st.ug.edu.gh>
2024-05-19 18:30:51 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,4 @@
+// TASK 1
 function arrayManipulation(arr) {
     return arr.map((num) => {
       if (num % 2 === 0) {
@@ -11,3 +12,21 @@ function arrayManipulation(arr) {
   const inputArray = [9, 4, 0, 5];
   const results = arrayManipulation(inputArray);
   console.log(results);
+
+// Task 2
+function formatArrayStrings(strArr, numArr) {
+    return strArr.map((str, index) => {
+      const num = numArr[index];
+      if (num % 2 === 0) {
+        return str.toUpperCase(); // Capitalize if number is even
+      }
+      return str.toLowerCase(); // Convert to lowercase if number is odd
+    });
+  }
+  
+  //solution
+  export const names = ["Fred", "Alberta", "Alice", "Cecil"];
+  export const modifiedNames = formatArrayStrings(names, results);
+  console.log(modifiedNames);
+
+  
